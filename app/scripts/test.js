c6532aeba48ea5adf25dc9bf2429ba6f1700d1e3
Manuel Sabogal <mfer32@gmail.com>
2016-05-16 13:07:23 -0500

    Removed TypeScript compiled files and added them to the .gitignore.

--- app/scripts/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var React = require('react');
-var ReactDOM = require('react-dom');
-var components_1 = require('./components');
-var data = [
-    { key: 1, name: "Pete Hunt" },
-    { key: 2, name: "Jordan Walke" }
-];
-ReactDOM.render(React.createElement(components_1.UserList, {data: data}), document.getElementById('test'));
